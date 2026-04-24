a02ec2d93688554a146de8f23994f38862e1c17e
danielhufnagle <114324335+danielhufnagle@users.noreply.github.com>
2026-04-24 09:47:13 -0500

    modified testbench

--- test/tb.v
+++ test/tb.v
@@ -17,19 +17,11 @@ module tb ();
   reg clk;
   reg rst_n;
   reg ena;
-  reg [7:0] ui_in;
-  reg [7:0] uio_in;
-  wire [7:0] uo_out;
-  wire [7:0] uio_out;
-  wire [7:0] uio_oe;
+  wire[15:0] out; 
 
   // Replace tt_um_example with your module name:
-  tt_um_danielhufnagle_vga user_project (
-      .ui_in  (ui_in),    // Dedicated inputs
-      .uo_out (uo_out),   // Dedicated outputs
-      .uio_in (uio_in),   // IOs: Input path
-      .uio_out(uio_out),  // IOs: Output path
-      .uio_oe (uio_oe),   // IOs: Enable path (active high: 0=input, 1=output)
+  tt_um_danielhufnagle_rng user_project (
+      .out    (out),
       .ena    (ena),      // enable - goes high when design is selected
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
