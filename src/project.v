318e93825607b0b3f6f8dda45996e140d0a434c5
danielhufnagle <114324335+danielhufnagle@users.noreply.github.com>
2026-04-24 16:12:24 -0500

    fixed pinout

--- src/project.v
+++ src/project.v
@@ -6,25 +6,24 @@
 `default_nettype none
 
 module tt_um_danielhufnagle_rng(
-  input  wire [7:0] ui_in,    // Dedicated inputs
-  output wire [7:0] uo_out,   // Dedicated outputs
-  input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
-  output wire [7:0] uio_out,  // IOs: Bidirectional Output path
-  output wire [7:0] uio_oe,   // IOs: Bidirectional Output enable (active high)
-  input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-  input  wire       clk,      // clock
-  input  wire       rst_n     // reset_n - low to reset
+  input  wire [7:0] ui_in,
+  output wire [7:0] uo_out,
+  input  wire [7:0] uio_in,
+  output wire [7:0] uio_out,
+  output wire [7:0] uio_oe,
+  input  wire       ena,
+  input  wire       clk,
+  input  wire       rst_n
 );
-  reg [15:0] lfsr;
+  reg [7:0] lfsr;
   
   always @(posedge clk or negedge rst_n) begin
     if (!rst_n)
-      lfsr <= 16'hF;
+      lfsr <= 8'hF;
     else
-      lfsr <= {lfsr[14:0], lfsr[3] ^ lfsr[2]}; // linear feedback shift register
+      lfsr <= {lfsr[6:0], lfsr[3] ^ lfsr[2]}; // linear feedback shift register
   end
   
-  // Output the lower 8 bits of the LFSR
   assign uo_out = lfsr[7:0];
   
   // Unused outputs
