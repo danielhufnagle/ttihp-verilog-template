23fb7efe67342390db52341d48f2b8f2e65b0aca
danielhufnagle <114324335+danielhufnagle@users.noreply.github.com>
2026-04-24 09:59:21 -0500

    this time for sure again

--- src/project.v
+++ src/project.v
@@ -6,15 +6,28 @@
 `default_nettype none
 
 module tt_um_danielhufnagle_rng(
+  input  wire [7:0] ui_in,    // Dedicated inputs
+  output wire [7:0] uo_out,   // Dedicated outputs
+  input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+  output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+  output wire [7:0] uio_oe,   // IOs: Bidirectional Output enable (active high)
   input  wire       ena,      // always 1 when the design is powered, so you can ignore it
   input  wire       clk,      // clock
-  input  wire       rst_n,     // reset_n - low to reset
-  output reg [15:0] out       // output
+  input  wire       rst_n     // reset_n - low to reset
 );
+  reg [15:0] lfsr;
+  
   always @(posedge clk or negedge rst_n) begin
-        if (!rst_n)
-            out <= 16'hF;
-        else
-            out <= {out[14:0], out[3] ^ out[2]}; // linear feedback shift register
-    end
+    if (!rst_n)
+      lfsr <= 16'hF;
+    else
+      lfsr <= {lfsr[14:0], lfsr[3] ^ lfsr[2]}; // linear feedback shift register
+  end
+  
+  // Output the lower 8 bits of the LFSR
+  assign uo_out = lfsr[7:0];
+  
+  // Unused outputs
+  assign uio_out = 8'b0;
+  assign uio_oe = 8'b0;
 endmodule
